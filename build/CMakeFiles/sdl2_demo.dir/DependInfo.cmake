dc95e8ede749721ed0dc3d2018e45a91e1e69bf4
samuebla <samuebla@ucm.es>
2022-05-28 13:32:19 +0200

    adaptada build a ambas maquinas virtuales

--- build/CMakeFiles/sdl2_demo.dir/DependInfo.cmake
+++ build/CMakeFiles/sdl2_demo.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/xherion/FinalRedes4/src/main.cpp" "/home/xherion/FinalRedes4/build/CMakeFiles/sdl2_demo.dir/src/main.cpp.o"
+  "/home/sam/Escritorio/ProyectoFinalRedes/src/main.cpp" "/home/sam/Escritorio/ProyectoFinalRedes/build/CMakeFiles/sdl2_demo.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/xherion/FinalRedes4
+# Install script for directory: /home/sam/Escritorio/ProyectoFinalRedes
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/xherion/FinalRedes4/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/sam/Escritorio/ProyectoFinalRedes/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
